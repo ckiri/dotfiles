5eeab2063427ba3dd4890d3dfccb1f81eb9da5f0
Chris Kiriakou <chris-kiriakou@web.de>
2023-08-30 17:37:40 +0200

    fixed weather formation bug

--- weather/.local/bin/weather/weather.sh
+++ weather/.local/bin/weather/weather.sh
@@ -5,10 +5,10 @@
 get_weather() {
   while :; do
     local wttr
-    read wttr "wttr.in/Besigheim?format=1" 
+    wttr=$(curl "wttr.in/?format=1")
 
     if [[ -n "$wttr" ]]; then 
-      echo $wttr > $HOME/.local/bin/weather/weather.txt
+      echo "$wttr" > $HOME/.local/bin/weather/weather.txt
     fi
     sleep 1800
   done
