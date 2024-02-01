a580a815ad303dc8c032dea2aca626ccc33d6a38
Chris Kiriakou <chris-kiriakou@web.de>
2024-02-01 10:34:28 +0100

    Create a passmenu clone with wofi.
    Implemented error handling by displaying a notification in dunst
    if script is used in X11.
    
    Added documentation of each step.
    
    Implemented wofi in dmenu mode.

---
+++ passmenu/.local/bin/passmenu/passmenu.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+#
+# Select a password from `pass` using wofi instead of dmenu.
+# Mocks the standart implementation of passmenu. 
+
+symbol="/usr/share/icons/Papirus-Dark/symbolic/status/dialog-password-symbolic.svg"
+
+# Check if a wayland compositor is used, if not throw error
+if [[ -n $WAYLAND_DISPLAY ]]; then
+	wofi="wofi --show dmenu"
+else
+	notify-send -u normal "No wayland compositor used" -i $symbol
+	exit 
+fi
+
+# Select all passwords in password store
+password_files=$(find ~/.password-store -type f -name '*.gpg' -exec sh -c 'basename {} .gpg' \;)
+
+# Pipe all passwords into wofi using dmenu mode for selection
+password=$(printf '%s\n' "${password_files}" | $wofi "$@")
+
+# Exit if store is empry
+[[ -n $password ]] || exit
+
+# Copy selected password into clipboard
+pass show --clip $password
