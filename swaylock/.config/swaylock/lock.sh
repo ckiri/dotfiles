e6397cdd8289201fd3866c36ddefadd0505466f7
Chris Kiriakou <chris-kiriakou@web.de>
2024-02-16 12:01:21 +0100

    Changed colors.

--- swaylock/.config/swaylock/lock.sh
+++ swaylock/.config/swaylock/lock.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+#
+# Simple script for screen-locking with swaylock. 
 
+# Run swaylock with the following styling options
 swaylock \
   --indicator-radius 160 \
   --indicator-thickness 20 \
@@ -10,19 +13,19 @@ swaylock \
   --key-hl-color "$color1" \
   --bs-hl-color "$color2" \
   --ring-color "$background" \
-  --ring-clear-color cdcd00FF \
-  --ring-wrong-color cd0000FF \
-  --ring-ver-color 0000cdFF \
+  --ring-clear-color F8E45CFF \
+  --ring-wrong-color ED333BFF \
+  --ring-ver-color 51A1FFFF \
   --line-uses-ring \
   --line-color 00000000 \
-  --font 'Dejavu Sans 40' \
-  --text-color EEEEEEFF \
-  --text-clear-color EEEEEEFF \
-  --text-wrong-color EEEEEEFF \
-  --text-ver-color EEEEEEFF \
+  --text-color ffffffff \
+  --text-clear-color ffffffff \
+  --text-wrong-color ffffffff \
+  --text-ver-color ffffffff \
   --separator-color 00000000 \
-  --color 000000FF \
+  --color 1E1E1EFF \
   --daemonize
 
+# Turn off display after 2 seconds
 sleep 2
 hyprctl dispatch dpms off
