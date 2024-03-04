49cfa17d9ffdc9ff192a8dddf6872607c94ddc3d
Chris Kiriakou <chris-kiriakou@web.de>
2024-03-04 17:12:49 +0100

    Changed script to use tofi instead of wofi.

--- passmenu/.local/bin/passmenu/passmenu.sh
+++ passmenu/.local/bin/passmenu/passmenu.sh
@@ -7,7 +7,7 @@ symbol="/usr/share/icons/Papirus-Dark/symbolic/status/dialog-password-symbolic.s
 
 # Check if a wayland compositor is used, if not throw error
 if [[ -n $WAYLAND_DISPLAY ]]; then
-	tofi="wofi --show dmenu"
+	tofi="tofi"
 else
 	notify-send -u normal "No wayland compositor used" -i $symbol
 	exit 
