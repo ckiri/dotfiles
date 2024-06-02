7c55c870f15a530c9b677428959d8012ab418b26
Chris Kiriakou <chris-kiriakou@web.de>
2024-06-02 19:20:16 +0200

    Removed unused configs.

--- hypridle/.local/bin/hypridle/hypridle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Idle script, turns off display after 10 min
-# Then suspends system to save energy
-
-hypridle \
-timeout 600 'hyprctl dispatch dpms off' \
-resume 'hyprctl dispatch dpms on' \
-timeout 1800 'systemctl suspend'
