773d1ea0190dae21632bccb2f1f754e2592523cf
Chris Kiriakou <chris-kiriakou@web.de>
2024-06-29 08:53:57 +0200

    Added runmenu.

---
+++ runmenu/.local/bin/runmenu/runmenu.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+bemenu-run \
+  --prompt "run:" \
+  --hp 8 \
+  --vim-normal-mode \
+  --vim-esc-exit \
+  --fixed-height \
+  --fn monospace,9 \
+  --ff '#bbbbbb' \
+  --fb '#222222' \
+  --tf '#bbbbbb' \
+  --tb '#222222' \
+  --nb '#222222' \
+  --ab '#222222' \
+  --hb '#005577' \
+  --hf '#eeeeee' \
+  --nf '#bbbbbb' \
+  --af '#bbbbbb'
