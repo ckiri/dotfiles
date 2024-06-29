4d2b37338844f5aa9dc8a0285c0e6eafad593576
Chris Kiriakou <c.m.kiriakou@googlemail.com>
2024-06-29 14:00:27 +0200

    Changed menu from wofi to bemenu.

--- passmenu/.local/bin/passmenu/passmenu.sh
+++ passmenu/.local/bin/passmenu/passmenu.sh
@@ -7,7 +7,21 @@ symbol="/usr/share/icons/Papirus-Dark/symbolic/status/dialog-password-symbolic.s
 
 # Check if a wayland compositor is used, if not throw error
 if [[ -n $WAYLAND_DISPLAY ]]; then
-	menu="wofi --show dmenu"
+	menu=" \
+		bemenu \
+		--prompt passmenu: \
+		--hp 8 \
+		--fn monospace,9 \
+		--ff #bbbbbb \
+		--fb #222222 \
+		--tf #bbbbbb \
+		--tb #222222 \
+		--nb #222222 \
+		--ab #222222 \
+		--hb #005577 \
+		--hf #eeeeee \
+		--nf #bbbbbb \
+		--af #bbbbbb"
 else
 	notify-send -u normal "No wayland compositor used" -i $symbol
 	exit 
@@ -16,7 +30,7 @@ fi
 # Select all passwords in password store
 password_files=$(find ~/.password-store -type f -name '*.gpg' -exec sh -c 'basename {} .gpg' \;)
 
-# Pipe all passwords into wofi using dmenu mode for selection
+# Pipe all passwords into the menu
 password=$(printf '%s\n' "${password_files}" | $menu "$@")
 
 # Exit if store is empry
