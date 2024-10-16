fa9cd07c94710c4ac70e5a1004782a05d038287b
Chris Kiriakou <chris-kiriakou@web.de>
2024-10-16 19:35:31 +0200

    Updates.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,7 +11,7 @@ vim.opt.relativenumber = true
 vim.opt.mouse = "a"
 vim.opt.showmode = true
 vim.opt.clipboard = "unnamedplus"
--- vim.opt.clipboard:append("unnamedplus")
+--vim.opt.clipboard:append("unnamedplus")
 vim.opt.termguicolors = false
 vim.opt.breakindent = true
 vim.opt.undofile = true
@@ -38,6 +38,8 @@ vim.opt.showtabline = 2
 vim.opt.background = "dark"
 vim.opt.wrap = false
 vim.opt.cursorline = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 -- [[ Basic Keymaps ]]
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
@@ -55,202 +57,19 @@ vim.keymap.set("n", "<C-h>", ":tabprev<CR>", { noremap = true, silent = true, de
 vim.keymap.set("n", "<C-j>", ":bnext<CR>", { noremap = true, silent = true, desc = "Move to next buffer" })
 vim.keymap.set("n", "<C-k>", ":bprev<CR>", { noremap = true, silent = true, desc = "Move to previous buffer" })
 
--- [[ Basic Autocommands ]]
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
+require('lsp_config')
 
--- [[ `lazy.nvim` plugin manager ]]
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
-	-- Own personal wiki
-	{
-		"vimwiki/vimwiki",
-		init = function()
-			vim.g.vimwiki_list = {
-				{
-					path = "~/documents/vimwiki",
-				},
-			}
-		end,
-	},
-	"tpope/vim-sleuth",
-
-	-- LSP Configuration & Plugins
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{ "williamboman/mason.nvim", config = true },
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-			{ "j-hui/fidget.nvim",       opts = {} },
-			{ "folke/neodev.nvim",       opts = {} },
-		},
-		config = function()
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-				callback = function(event)
-					local map = function(keys, func, desc)
-						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-					end
-					map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-					map("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
-					map("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-					map("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-					map("<leader>ds", vim.lsp.buf.document_symbol, "[D]ocument [S]ymbols")
-					map("<leader>ws", vim.lsp.buf.workspace_symbol, "[W]orkspace [S]ymbols")
-					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-					map("K", vim.lsp.buf.hover, "Hover Documentation")
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.server_capabilities.documentHighlightProvider then
-						local highlight_augroup =
-								vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.document_highlight,
-						})
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.clear_references,
-						})
-						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-							callback = function(event2)
-								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-							end,
-						})
-					end
-					if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-						map("<leader>th", function()
-							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-						end, "[T]oggle Inlay [H]ints")
-					end
-				end,
-			})
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-			local servers = {
-				-- pyright = {},
-			}
-			require("mason").setup({
-				ui = {
-					window = {
-						border = "rounded",
-					},
-					icons = {
-						package_installed = "[i]",
-						package_pending = "[p]",
-						package_uninstalled = "[un]",
-					},
-				},
-			})
-			local ensure_installed = vim.tbl_keys(servers or {})
-			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-					end,
-				},
-			})
-		end,
-	},
-
-	-- Autocompletion
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			{
-				"L3MON4D3/LuaSnip",
-				build = (function()
-					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-						return
-					end
-					return "make install_jsregexp"
-				end)(),
-				dependencies = {},
-			},
-			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path",
-		},
-		config = function()
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-			luasnip.config.setup({})
-			cmp.setup({
-				window = {
-					completion = cmp.config.window.bordered({border = { "┌", "─", "┐", "│", "┘", "─", "└", "│"}}),
-					documentation = cmp.config.window.bordered({border = { "┌", "─", "┐", "│", "┘", "─", "└", "│"}}),
-				},
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				completion = { completeopt = "menu,menuone,noinsert" },
-				mapping = cmp.mapping.preset.insert({
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					["<C-p>"] = cmp.mapping.select_prev_item(),
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete({}),
-					["<C-l>"] = cmp.mapping(function()
-						if luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
-						end
-					end, { "i", "s" }),
-					["<C-h>"] = cmp.mapping(function()
-						if luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						end
-					end, { "i", "s" }),
-				}),
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "path" },
-				},
-			})
-		end,
-	},
-}, {})
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	underline = true,
-	virtual_text = false,
-	signs = true,
-	update_in_insert = false,
-})
-
+-- [[ Colorscheme  ]]
 vim.cmd("hi TabLineSel ctermbg=black ctermfg=white")
 vim.cmd("hi TabLine ctermbg=darkgrey ctermfg=black")
 vim.cmd("hi ColorColumn ctermbg=black ctermfg=grey")
 vim.cmd("hi LineNr ctermfg=darkgrey")
 vim.cmd("hi PmenuSel ctermfg=darkgrey ctermbg=yellow")
-vim.cmd("hi CursorLine ctermfg=lightcyan term=none cterm=none")
+vim.cmd("hi CursorLine term=none cterm=none")
 vim.cmd("hi CursorLineNr ctermfg=yellow")
 vim.cmd("hi ModeMsg ctermfg=lightblue")
 vim.cmd("hi MsgArea ctermfg=lightmagenta")
 vim.cmd("set path+=**")
 vim.cmd("set guicursor=")
 vim.cmd("syntax off")
-vim.cmd("filetype off")
+--vim.cmd("filetype off")
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,3 +1,21 @@
 #!/bin/sh
 
-[ -z "$SSH_CLIENT" ] && [ -z "$TMUX" ] && [ -z "$DISPLAY" ] && Hyprland
+select_env() {
+  printf "Select a startup option:\n Labwc(1), Hyprland(2) or TTY(*)?\n"
+  printf "(Default is TTY.)\n"
+  printf "> "
+  read choice
+  printf "\n"
+
+  if [ "$choice" = "1" ]; then
+    printf "Starting Labwc\n"
+    labwc
+  elif [ "$choice" = "2" ]; then
+    printf "Starting Hyprland\n"
+    Hyprland
+  else
+    printf "Stay in TTY\n"
+  fi
+}
+
+[ -z "$SSH_CLIENT" ] && [ -z "$TMUX" ] && [ -z "$DISPLAY" ] && select_env
