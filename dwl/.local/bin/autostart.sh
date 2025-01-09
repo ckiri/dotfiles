9c85e36945dd7bcd9c3a8ab8031acb60f435522b
Chris Kiriakou <chris-kiriakou@web.de>
2025-01-09 08:17:20 +0100

    Updates.

--- dwl/.local/bin/autostart.sh
+++ dwl/.local/bin/autostart.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 #
 # Autostart apps for use in dwl
-
 kanshi 2>&1 &
 gammastep -l 49.0:9.2 -t 5700:3600 -b 1:0.75 >/dev/null 2>&1 &
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' 2>&1 &
-gsettings set org.gnome.desktop.interface icon-theme 'Pairus-Dark' 2>&1 &
+gsettings set org.gnome.desktop.interface gtk-theme '' 2>&1 &
+gsettings set org.gnome.desktop.interface icon-theme '' 2>&1 &
 dunst 2>&1 &
 swayidle -w \
     timeout 300 'brightnessctl set 1%' resume 'brightnessctl --restore' \
--- dwl/.local/bin/startdwl.sh
+++ dwl/.local/bin/startdwl.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 #
 # Startup script for dwl
-
 while true; do
 	#dwm 2> ~/.dwm.log # error logging
-    statusbar.sh | dwl -s autostart.sh >/dev/null 2>&1
+    dwl -s autostart.sh >/dev/null 2>&1
 done
