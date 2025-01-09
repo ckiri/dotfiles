4ef1594873dc184a2232ea52bf0c94df1ffaad03
Chris Kiriakou <chris-kiriakou@web.de>
2025-01-09 23:50:18 +0100

    Fix bug in script.

--- weather/.local/bin/weather.sh
+++ weather/.local/bin/weather.sh
@@ -2,12 +2,9 @@
 #
 # Fetch weather information from wttr.in
 
-# Request wttr.in API, then sleep for 30 min
-get_weather() {
-    while true
-    do
-        wttr=$(curl "wttr.in/~?format=%C+%t") \
-                && printf "$wttr\n" > $HOME/.cache/weather/weather
-        sleep 1800
-    done
-}
+while true
+do
+    wttr=$(curl "wttr.in/~?format=%C+%t") \
+            && printf "$wttr\n" > $HOME/.cache/weather/weather
+    sleep 1800
+done
