53302b85870ecf458182431b4c2d2e8677b3b86a
Chris Kiriakou <chris-kiriakou@web.de>
2025-01-21 22:16:42 +0100

    Start niri with the session flag.

--- scripts/.local/bin/startup.sh
+++ scripts/.local/bin/startup.sh
@@ -10,10 +10,10 @@ kanshi >/dev/null 2>&1 &
 gammastep -l 49.0:9.2 -t 5700:3600 >/dev/null 2>&1 &
 gsettings set org.gnome.desktop.interface gtk-theme '' 2>&1 &
 gsettings set org.gnome.desktop.interface icon-theme '' 2>&1 &
-swayidle -w \
-    timeout 300 'brightnessctl --save && brightnessctl set 1%' resume 'brightnessctl --restore' \
-    timeout 600 'wlopm --off \*' resume 'wlopm --on \*' \
-    timeout 1800 'systemctl suspend-then-hibernate' >/dev/null 2>&1 &
+//swayidle -w \
+//    timeout 300 'brightnessctl --save && brightnessctl set 1%' resume 'brightnessctl --restore' \
+//    timeout 600 'wlopm --off \*' resume 'wlopm --on \*' \
+//    timeout 1800 'systemctl suspend-then-hibernate' >/dev/null 2>&1 &
 mpd >/dev/null 2>&1 &
 dbus-update-activation-environment \
     --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP >/dev/null 2>&1 &
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 [[ -z "$SSH_CLIENT" ]] && [[ -z "$TMUX" ]] && [[ -z "$DISPLAY" ]] \
-    && dwl -s ~/.local/bin/startup.sh >/dev/null 2>&1
+    && niri --session >/dev/null 2>&1
