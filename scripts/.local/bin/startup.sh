93ad17fe55c38e941cc1da53d5e4551c6daefaf4
Chris Kiriakou <chris-kiriakou@web.de>
2025-02-17 19:27:32 +0100

    Updates.

--- scripts/.local/bin/startup.sh
+++ scripts/.local/bin/startup.sh
@@ -1,17 +1,30 @@
 #! /bin/sh
 #
-# Start processes when starting a compositor/window manager
-dunst 2>&1 &
-kanshi >/dev/null 2>&1 &
-test -d /sys/class/power_supply/BAT1 && $HOME/.local/bin/battery.sh 2>&1 &
-gammastep -l 49.0:9.2 -t 5700:3600 >/dev/null 2>&1 &
-dconf write /org/gnome/desktop/interface/color-scheme '"prefer-light"' 2>&1 &
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita' 2>&1 &
-gsettings set org.gnome.desktop.interface icon-theme 'Papirus' 2>&1 &
+# Start applications for use with a wayland compositor
+
+kanshi >/dev/null 2>&1 & # The video output configuration daemon
+
+# Check if device has a battery, if true, start a battery daemon
+if test -d /sys/class/power_supply/BAT1
+then
+    $HOME/.local/bin/batterystate.sh 2>&1 &
+fi
+
+gammastep -l 49.0:9.2 -t 5700:3600 >/dev/null 2>&1 & # Enable eyesaving mode
+
+# Set dark theme for applications
+dconf write /org/gnome/desktop/interface/color-scheme '"prefer-dark"' 2>&1 & 
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' 2>&1 & 
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' 2>&1 &
+
+# Start the idle daemon
 swayidle -w \
     timeout 300 'brightnessctl --save && brightnessctl set 1%' resume 'brightnessctl --restore' \
-    timeout 600 'wlopm --off \*' resume 'wlopm --on \*' \
-    timeout 1800 'systemctl suspend-then-hibernate' >/dev/null 2>&1 &
-mpd >/dev/null 2>&1 &
+    timeout 600 'niri msg output off \*' resume 'niri msg output on \*' \
+    timeout 900 'swaylock -f' \
+    timeout 1800 'systemctl suspend-then-hibernate' \
+    before-sleep 'swaylock -f' >/dev/null 2>&1 &
+
+mpd >/dev/null 2>&1 & # Start the music player daemon
 dbus-update-activation-environment \
-    --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP >/dev/null 2>&1 &
+    --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP >/dev/null 2>&1 & # DBus activation (Needed for keyring callbacks)
